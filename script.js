4c8f931d7ee0322e8a0c17bb9d7fead9776e5c97
Bunditworld <bdw.learning1@gmail.com>
2025-10-08 11:52:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         .catch(() => {
-            // ไม่แสดงข้อความผิดพลาดใด ๆ ตามที่ขอ
         });
 
     function displayInitialMessage() {
